c13a13c2723428c68d12a1598db56ce28389aeb9
Shubham Gupta <shubham15gupta09@gmail.com>
2020-05-26 01:43:41 +0530

    post method working

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express" );
 const app = express() ;
+const bodyParser = require("body-parser") ;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({})) ;
 
 app.get("/" , ( req , res ) => {
     res.sendFile( __dirname + "/Index/home.html") ;
@@ -10,6 +14,14 @@ app.get("/Index/post.html" , ( req , res ) => {
 app.get("/Index/view.html" , ( req , res ) => {
     res.sendFile( __dirname + "/Index/view.html") ;
 }) ;
+
+app.post("/data" , (req,res)=>{
+    res.send({
+        message : "Got the information" ,
+        data : req.body,
+        Go_to_home : "http://localhost:3000/"
+    });
+});
 
 app.listen( 3000 , () => {
     console.log("Server started at http://localhost:3000/")
